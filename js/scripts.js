0937aa35738a7283cb41938a5bf0abac39a03b9f
Kevin Williams <kevin@k9w.org>
2022-01-26 15:32:57 -0800

    Refactor scripts.js code to work better.
    Co-Authored-By: Brenna Lavin <lavinbrenna@gmail.com>
        Co-authored-by: Will W. <wjwat@onionslice.org>
        Co-authored by: Andy Plymate <Andy.Plymate@gmail.com>
        Co-Authored-By: Mikah Mathews <mikah@outofband.org>

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,49 @@
+// Business Logic
 
-
-// Only 2 players for our games right now.
 function Game() {
-  this.players = [];
-  // this.currentRound = 0;
-}
-
-Game.prototype.startGame = function() {
   let player1 = new Player();
   let player2 = new Player();
-  this.players.push(player1, player2);
-  this.currentTurn = 1; 
+  this.players = [player1, player2];
+  this.currentPlayer = 0;
 }
 
-Game.prototype.winCheck = function(player1,player2) {
-  if(player1.totalScore === 100) {
-    return "Player 1 wins!";
-  } else if(player2.totalScore === 100) {
-    return "Player 2 wins!";
+Game.prototype.resetGame = function() {
+  let player1 = new Player();
+  let player2 = new Player();
+  this.players = [player1, player2];
+  this.currentPlayer = 0;
+}
+
+Game.prototype.playRound = function() {
+  let newRoll = this.players[this.currentPlayer].roll();
+  console.log(newRoll);
+
+  if (newRoll === 1) {
+    this.players[this.currentPlayer].turnScore = 0;
+    this.endRound();
   } else {
-    currentTurn += 1;
+    this.players[this.currentPlayer].turnScore += newRoll;
   }
 }
 
-Game.prototype.currentPlayer = function() {
-  if (this.currentTurn % 2 === 0) {
-    return player1;
+Game.prototype.endRound = function() {
+  let playerCheck = this.currentPlayer + 1;
+
+  if (playerCheck > this.players.length - 1) {
+    playerCheck = 0;
+  }
+
+  this.players[this.currentPlayer].totalScore += this.players[this.currentPlayer].turnScore;
+  this.players[this.currentPlayer].turnScore = 0;
+  this.currentPlayer = playerCheck;
+}
+
+Game.prototype.isCurrentPlayerWinner = function() {
+  if (this.players[this.currentPlayer].totalScore + this.players[this.currentPlayer].turnScore >= 100) {
+    this.resetGame();
+    return true;
   } else {
-    return player2;
+    return false;
   }
 }
 
@@ -41,26 +57,12 @@ Player.prototype.roll = function() {
   // min will always be 1
   let min = 1;
   let max = 6;
-  // Math.random * 6 will generate a number between 0 and 6 (will never actually
-  // be 6 because it will be a decimal number), following order of operations,
-  // it will multiply the number we have set as the max first, and then add the
-  // min. 
 
-  let dieVal = Math.floor(Math.random() * max + min);
-  if (dieVal === 1) {
-    turnScore = 0;
-    //*pass turn*
-  } else {
-    turnScore += dieVal;
-  }
-  return turnScore;
-};
-
-Player.prototype.hold = function() {
-  // call winCheck
-  return totalScore += turnScore;
+  return Math.floor(Math.random() * max + min);
 }
 
-// let player1 = new Player();
-// let player2 = new Player();
+
+// User Interface Logic
+
+
 
